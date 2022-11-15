40e6496d52b58d4acb20b8dfc60ee33bcbe9ec96
Gilberto Jr <giljrsantos@gmail.com>
2022-11-15 20:51:40 -0300

    Setupe inicial do canvas; Desenhar o Flappy Bird na tela; Game Loop; Desenhar o resto do cenário

---
+++ README.md
@@ -0,0 +1,7 @@
+# ![DevSuperior logo](https://raw.githubusercontent.com/devsuperior/bds-assets/main/ds/devsuperior-logo-small.png) Jogo - Flappy Bird
+
+## Aula 01
+1. Setupe inicial do canvas;
+2. Desenhar o Flappy Bird na tela;
+3. Game Loop;
+4. Desenhar o resto do cenário;
---
+++ jogo.js
@@ -0,0 +1,94 @@
+console.log('[Gilberto Jr] Flappy Bird');
+
+const sprites = new Image();
+sprites.src = './sprites.png';
+
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
+
+// PLANO DE FUNDO
+const planoDeFundo = {
+    sprintX: 390,
+    sprintY: 0,
+    largura: 275,
+    altura: 204,
+    x: 0,
+    y: canvas.height - 204,
+
+    desenha(){
+        contexto.drawImage(
+            sprites,
+            planoDeFundo.sprintX, planoDeFundo.sprintY,
+            planoDeFundo.largura, planoDeFundo.altura,
+            planoDeFundo.x, planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura
+        );
+        contexto.drawImage(
+            sprites,
+            planoDeFundo.sprintX, planoDeFundo.sprintY,
+            planoDeFundo.largura, planoDeFundo.altura,
+            (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura
+        );
+    },
+};
+
+// CHÃO
+const chao ={
+    sprintX: 0,
+    sprintY: 610,
+    largura: 224,
+    altura: 112,
+    x: 0,
+    y: canvas.height - 112,
+
+    desenha(){
+        contexto.fillStyle = '#70c5ce';
+        contexto.fillRect(0,0, canvas.width, canvas.height)
+        contexto.drawImage(
+            sprites,
+            chao.sprintX, chao.sprintY,
+            chao.largura, chao.altura,
+            chao.x, chao.y,
+            chao.largura, chao.altura
+        );
+        contexto.drawImage(
+            sprites,
+            chao.sprintX, chao.sprintY,
+            chao.largura, chao.altura,
+            (chao.x + chao.largura), chao.y,
+            chao.largura, chao.altura
+        );
+    },
+
+};
+
+// Passarinho
+const flappyBird = {
+    sprintX: 0,
+    sprintY: 0,
+    largura: 33,
+    altura: 24,
+    x: 10,
+    y: 50,
+
+    desenha(){
+        contexto.drawImage(
+            sprites,
+            flappyBird.sprintX, flappyBird.sprintY, // Sprite X, Sprite Y
+            flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
+            flappyBird.x, flappyBird.y,
+            flappyBird.largura, flappyBird.altura,
+        );
+    }
+}
+
+function loop(){
+    planoDeFundo.desenha()
+    chao.desenha();
+    flappyBird.desenha()
+   
+    requestAnimationFrame(loop);
+}
+
+loop();
